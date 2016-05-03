96ead3846544871eec8183be024e0643f7e42691
David Schovanec <david@schovi.cz>
2016-05-03 15:19:36 +0200

    Change default defer value to 'true'

--- src/react-iscroll.jsx
+++ src/react-iscroll.jsx
@@ -48,7 +48,7 @@ export default class ReactIScroll extends React.Component {
   static propTypes = propTypes;
 
   static defaultProps = {
-    defer: 0,
+    defer: true,
     options: {},
     style: {
       position: "relative",
